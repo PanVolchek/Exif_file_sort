d94c8f2b6760653c5756f6995652253100c58a2c
Dmitriy Volkov <panvolchek@gmail.com>
2022-08-11 10:20:47 +0300

    Update Extract_exif_date.py

--- Extract_exif_date.py
+++ Extract_exif_date.py
@@ -1,11 +1,13 @@
 import os
 import exifread
 import time
-""" Read conten of folder
-Scan filename
-Scan EXIF creation date
-Compare
-Amend if necessary"""
+""" 
+1. Read conten of folder
+2. Scan filename
+3. Scan EXIF creation date
+4. Compare
+5. Amend if necessary
+"""
 for (root,dirs,files) in os.walk('C:\\Users\\UUser\\Documents\\Projects\\Python\\Test\\PicScan'):
     for filename in files:
         f = open( "DSCF9150.JPG", 'rb')
